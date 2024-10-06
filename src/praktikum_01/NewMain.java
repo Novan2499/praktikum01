529241079d9ce31cd3d4a5f15107126cc55b178e
Novan2499 <thecodingsistem@gmail.com>
2024-10-06 14:45:32 +0700

    first commit

---
+++ src/praktikum_01/NewMain.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package praktikum_01;
+import com.mysql.jdbc.Driver;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author USER
+ */
+public class NewMain {
+    
+}
+
+    
